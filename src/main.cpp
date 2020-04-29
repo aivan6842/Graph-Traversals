568b0508b4a5a95f74a179f6f0acb7f4f3872989
Alex Ivanov <alex.ivanov0615@gmail.com>
2020-04-29 13:05:00 -0400

    Added Depth First Search

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include "Graph.h"
 #include "Graph.cpp"
 #include <queue>
+#include <stack>
 
 using namespace std;
 
 template <class T>
-void bfs(T vertex, Graph<T> graph) {
+void bfs(T vertex, Graph<T>& graph) {
     // queue for bfs and set for checking if nodes have been visited
     queue<T> myQueue;
     set<T> visited;
@@ -35,6 +36,31 @@ void bfs(T vertex, Graph<T> graph) {
     }
 }
 
+template <class T>
+void dfs(T vertex, Graph<T>& graph) {
+    stack<T> stack;
+    set<T> visited;
+
+    stack.push(vertex);
+
+    while (!stack.empty()) {
+        T curr = stack.top();
+        stack.pop();
+        visited.insert(curr);
+        cout << "Visiting node:" << curr << endl;
+
+        for (T elem : graph.getNeighbours(curr)) {
+            class set<T> ::iterator it = visited.find(elem);
+
+            if (it == visited.end()) {
+                stack.push(elem);
+            }
+        }
+        
+    }
+ 
+}
+
 
 int main() {
     //initialize graph
@@ -55,7 +81,12 @@ int main() {
     graph1.addVertex(3, connections3);
 
     // perfrom BFS
+    cout << "Running Breadth First Search" << endl;
     bfs<int>(1, graph1);
+    cout << endl;
+    cout << "Running Breadth First Search" << endl;
+    dfs<int>(1, graph1);
+    cout << endl;
 
     Graph<string> graph2;
     set<string> connectionsS1 = { "Two", "Three" };
@@ -66,7 +97,13 @@ int main() {
     graph2.addVertex("Two", connectionsS2);
     graph2.addVertex("Three", connectionsS3);
 
+    cout << "Running Breadth First Search" << endl;
     bfs<string>("One", graph2);
+    cout << endl;
+
+    cout << "Running Depth First Search" << endl;
+    dfs<string>("One", graph2);
+    cout << endl;
 
     return 0;
 }
